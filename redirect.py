d86a414b293256ea648baf8933079757522f317a
A Farzat <a@farzat.xyz>
2022-05-28 11:22:52 +0900

    Add docstrings and format using black

--- redirect.py
+++ redirect.py
@@ -1,28 +1,37 @@
+#!/usr/bin/env python3
+
+"""qutebrowser-redirect
+
+This script redirects websites to their more privacy-respecting front-ends.
+
+To use the script, put it in `~/.config/qutebrowser/` and add
+`config.source('redirect.py')` in your `config.py` .
+"""
 import qutebrowser.api.interceptor
 
+
 def rewrite(request: qutebrowser.api.interceptor.Request):
-    if request.request_url.host () == 'www.reddit.com':
-        request.request_url.setHost ('teddit.net')
-
+    """Intercepts requests and redirects them to alternative host"""
+    if request.request_url.host() == "www.reddit.com":
+        request.request_url.setHost("teddit.net")
         try:
-            request.redirect (request.request_url)
-        except:
+            request.redirect(request.request_url)
+        except Exception:
             pass
 
-    if request.request_url.host () == 'www.instagram.com':
-        request.request_url.setHost ('bibliogram.snopyta.org')
-
+    if request.request_url.host() == "www.instagram.com":
+        request.request_url.setHost("bibliogram.snopyta.org")
         try:
-            request.redirect (request.request_url)
-        except:
+            request.redirect(request.request_url)
+        except Exception:
             pass
 
-    if request.request_url.host () == 'twitter.com':
-        request.request_url.setHost ('nitter.eu')
-
+    if request.request_url.host() == "twitter.com":
+        request.request_url.setHost("nitter.eu")
         try:
-            request.redirect (request.request_url)
-        except:
+            request.redirect(request.request_url)
+        except Exception:
             pass
+
 
 qutebrowser.api.interceptor.register(rewrite)
