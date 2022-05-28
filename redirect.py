98cea76f76d59da55652c3054a3d14f370c47d5d
A Farzat <a@farzat.xyz>
2022-05-28 18:25:32 +0900

    Make the script choose an instance at random
    This is better for privacy as no one instance has access to all your
    searches.
    
    Also refactor by turning the code into a loop.

--- redirect.py
+++ redirect.py
@@ -7,31 +7,64 @@ This script redirects websites to their more privacy-respecting front-ends.
 To use the script, put it in `~/.config/qutebrowser/` and add
 `config.source('redirect.py')` in your `config.py` .
 """
+from random import choice
 import qutebrowser.api.interceptor
+
+ALL_HOSTS = [
+    [
+        {
+            "www.youtube.com",
+            "youtube.com",
+            "youtu.be",
+        },
+        [
+            "invidious.snopyta.org",
+            "invidious.namazso.eu",
+            "yt.artemislena.eu",
+            "invidious.flokinet.to",
+            "invidious.weblibre.org",
+            "inv.riverside.rocks",
+            "y.com.sb",
+            "invidious.sethforprivacy.com",
+        ],
+    ],
+    [
+        {"www.reddit.com", "reddit.com"},
+        [
+            "libreddit.privacy.com.de",
+            "libredd.it",
+            "libreddit.some-things.org",
+            "libreddit.nl",
+            "leddit.xyz",
+        ],
+    ],
+    [
+        {"www.twitter.com", "twitter.com"},
+        [
+            "nitter.net",
+            "nitter.pussthecat.org",
+            "nitter.unixfox.eu",
+            "nitter.hu",
+            "nitter.it",
+            "nitter.sethforprivacy.com",
+            "nitter.cz",
+        ],
+    ],
+]
 
 
 def rewrite(request: qutebrowser.api.interceptor.Request):
     """Intercepts requests and redirects them to alternative host"""
-    if request.request_url.host() == "www.reddit.com":
-        request.request_url.setHost("teddit.net")
-        try:
-            request.redirect(request.request_url)
-        except Exception:
-            pass
-
-    if request.request_url.host() == "www.instagram.com":
-        request.request_url.setHost("bibliogram.snopyta.org")
-        try:
-            request.redirect(request.request_url)
-        except Exception:
-            pass
-
-    if request.request_url.host() == "twitter.com":
-        request.request_url.setHost("nitter.eu")
-        try:
-            request.redirect(request.request_url)
-        except Exception:
-            pass
+    for bad_hosts, alternatives in ALL_HOSTS:
+        if request.request_url.host() in bad_hosts:
+            request.request_url.setHost(choice(alternatives))
+            for _ in range(3):  # Try 3 times, just to make sure.
+                try:
+                    request.redirect(request.request_url)
+                    break
+                except Exception:
+                    pass
+            break
 
 
 qutebrowser.api.interceptor.register(rewrite)
